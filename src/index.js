81c03854c63cf69c759d5dc200ac310ddffe9239
Denis Davydov <macevoa@users.noreply.github.com>
2021-01-20 19:32:17 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, config) {
+    config = config.flat(1);
+    let pattern = '';
+    config.forEach((x, iter) => {
+      if (+x) {
+        pattern += x;
+      } else {
+        pattern += '\\' + x;
+      }
+      if (iter % 2) pattern += '|';
+    });
+    pattern = pattern.slice(0, -1);
+  
+    let newStr;
+    let reg = new RegExp(pattern, 'g');
+    while (str.length !== 0) {
+      newStr = str.replace(reg, '')
+      if (newStr === str) return false;
+      str = newStr;
+    }
+    return true;
+  }
